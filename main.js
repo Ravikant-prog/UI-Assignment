c9976a309db43bc4ce3cdd00d00b3d483728f361
Ravikant-prog <sravikant889@gmail.com>
2021-08-11 17:50:27 +0530

    filter setup

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
-let data
-const table = document.querySelector("table")
-const headKeys = ["name", "icao", "type"]
+let data;
+let types = {};
+const table = document.querySelector("table");
+const headKeys = ["name", "icao", "iata", "elevation", "type"];
 
 const getData = async () => {
-    const res = await fetch("./data/airports.json");
-    data = await res.json();
-    createHeads(headKeys);
-    populateTable(data)
-  };
+  const res = await fetch("./data/airports.json");
+  data = await res.json();
+  getTypes(data);
+  createTypesFilter();
+  createHeads(headKeys);
+  populateTable(data.splice(0, 10));
+};
 
-  const createHeads = (heads) => {
-    const thead = table.createTHead();
-    const row = thead.insertRow();
-    heads.forEach((head) => {
-      let th = document.createElement("th");
-      let text = document.createTextNode(head);
-      th.appendChild(text);
-      row.appendChild(th);
-    });
-  };
+const getTypes = (data) => {
+  data.forEach((ele) => {
+    if (!types[ele.type]) types[ele.type] = 1;
+  });
+};
 
-  const populateTable = (data) => {
-    data.forEach((ele) => {
-      let row = table.insertRow();
-      for (let key of headKeys) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(ele[key]);
-        cell.appendChild(text);
-      }
-    });
-  };
+const createTypesFilter = () => {
+  Object.keys(types).forEach((type) => {
+    const filterDiv = document.querySelector(".filters");
+    const checkbox = document.createElement("input");
 
-  getData();
+    checkbox.type = "checkbox";
+    checkbox.name = type;
+    checkbox.value = false;
+    checkbox.id = type;
+    checkbox.onchange = () => onFilterChange(type);
+    const label = document.createElement("label");
+
+    label.appendChild(document.createTextNode(type));
+    filterDiv.appendChild(checkbox);
+    filterDiv.appendChild(label);
+  });
+};
+
+const onFilterChange = (by) => {
+  console.log(by);
+};
+
+const createHeads = (heads) => {
+  const thead = table.createTHead();
+  const row = thead.insertRow();
+  heads.forEach((head) => {
+    let th = document.createElement("th");
+    let text = document.createTextNode(head);
+    th.appendChild(text);
+    row.appendChild(th);
+  });
+};
+
+const populateTable = (data) => {
+  data.forEach((ele) => {
+    let row = table.insertRow();
+    for (let key of headKeys) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(ele[key]);
+      cell.appendChild(text);
+    }
+  });
+};
+
+getData();
