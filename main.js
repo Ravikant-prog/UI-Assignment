87ce51b0fb00b7c04a5c4274769672898007798d
Ravikant-prog <sravikant889@gmail.com>
2021-08-11 15:20:43 +0530

    populating table

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+let data
+const table = document.querySelector("table")
+const headKeys = ["name", "icao", "type"]
+
 const getData = async () => {
     const res = await fetch("./data/airports.json");
     data = await res.json();
-    console.log(data[0])
+    createHeads(headKeys);
+    populateTable(data)
   };
+
+  const createHeads = (heads) => {
+    const thead = table.createTHead();
+    const row = thead.insertRow();
+    heads.forEach((head) => {
+      let th = document.createElement("th");
+      let text = document.createTextNode(head);
+      th.appendChild(text);
+      row.appendChild(th);
+    });
+  };
+
+  const populateTable = (data) => {
+    data.forEach((ele) => {
+      let row = table.insertRow();
+      for (let key of headKeys) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(ele[key]);
+        cell.appendChild(text);
+      }
+    });
+  };
+
   getData();
